6fe06f3c4d8cbdb77096aa00b213b5189513e4d3
Marllon Frizzo <mffrizzo@gmail.com>
2022-03-13 02:13:43 -0300

    adding getPost and creating dynamic route

---
+++ app/routes/posts/$slug.tsx
@@ -0,0 +1,21 @@
+import { json, useLoaderData } from 'remix';
+import type { LoaderFunction } from 'remix';
+import invariant from "tiny-invariant";
+
+import { getPost } from '~/post';
+import type { Post } from '~/post';
+
+export const loader: LoaderFunction = async ({ params }) => {
+    invariant(params.slug, "expected params.slug");
+    return json(await getPost(params.slug));
+}
+
+export default function PostSlug() {
+    const post = useLoaderData<Post>();
+
+    return (
+        <main>
+            <h1>{post.title}</h1>
+        </main>
+    );
+}
